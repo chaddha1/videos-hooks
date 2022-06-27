65a2963d571191585cc413511f85db4aaa962cd7
chaddha1 <himanshuchaddha4u@gmail.com>
2022-06-27 11:37:43 +0530

    videos with hooks done

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+import useVideos from "../hooks/useVideos";
 
-  componentDidMount(){
-    this.onTermSubmit('Marvel')
-  }
+const App = () => {
+  const [selectedVideo, setSelectedVideos] = useState(null);
+  const [videos, search] = useVideos("Marvel");
 
-  onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", { params: { q: term } });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
+  useEffect(() => {
+    setSelectedVideos(videos[0]);
+  }, [videos]);
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideos} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
